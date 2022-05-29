e7147fb85e813ca73538028ac33ea07a0ce6555c
Moises Alvarenga <moises.alvarenga@outlook.com>
2022-05-29 13:35:07 -0300

    Iniciando projeto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EnqueteWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
